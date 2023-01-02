e86d67b33d8a8e248789fba3b999f184997ceb03
anniechakraborty <the.demented.genius29@gmail.com>
2023-01-03 00:23:26 +0530

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InputFieldsComponent } from './input-fields/input-fields.component';
 import { OutputFieldsComponent } from './output-fields/output-fields.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputFieldsComponent,
-    OutputFieldsComponent
+    OutputFieldsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
