bbe2789bd06f7000a15eb94538e0e76575c1b8a8
anniechakraborty <the.demented.genius29@gmail.com>
2023-01-12 02:59:50 +0530

    getting input values and setting clear functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputFieldsComponent } from './input-fields/input-fields.component';
@@ -14,9 +15,12 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    
+}
--- src/app/input-fields/input-fields.component.ts
+++ src/app/input-fields/input-fields.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-fields.component.css']
 })
 export class InputFieldsComponent {
-  heatNumber = 0;
+  heat : string = "";
+  date : Date;
+  liqTemp : string = "";
+  preCastWGT : string = "";
+  castSteelWGT : string = "";
+  ldlLife : string = "";
+  timeSteel : string = "";
+  empty0 : string = "";
+  empty01 : string = "";
+  empty02 : string = "";
+  empty03 : string = "";
+  bombTemp : string = "";
+  tapToStartSec : string = "";
+  treatmDur : string = "";
+  castingDur : string = "";
+  lastTempToSec200MT : string = "";
+  facCd : string = "";
+  postFtcHT : string = "";
+  exitTemp : string = "";
+  postLitHT : string = "";
+  time200To25MT : string = "";
+
+  clearInput(){
+    this.heat = "";
+    this.date = null;
+    this.liqTemp = "";
+    this.preCastWGT = "";
+    this.castSteelWGT = "";
+    this.ldlLife = "";
+    this.timeSteel = "";
+    this.empty0 = "";
+    this.empty01 = "";
+    this.empty02 = "";
+    this.empty03 = "";
+    this.bombTemp = "";
+    this.tapToStartSec = "";
+    this.treatmDur = "";
+    this.castingDur = "";
+    this.lastTempToSec200MT = "";
+    this.facCd = "";
+    this.postFtcHT = "";
+    this.exitTemp = "";
+    this.postLitHT = "";
+    this.time200To25MT = "";
+  }
 }
