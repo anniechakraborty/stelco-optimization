bc3799abf4d1dd38c55228a30a998bb2e439551b
anniechakraborty <the.demented.genius29@gmail.com>
2023-02-14 12:06:04 +0530

    used mat formfield , mat label and mat input to redesign the input form

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialComponents = [
   MatButtonModule, 
   MatButtonToggleModule, 
-  MatIconModule
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
